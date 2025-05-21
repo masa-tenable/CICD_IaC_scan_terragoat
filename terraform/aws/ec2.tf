5d5d45be73bfbd43ab88abe34d32c20409901a3e
Masa Ito <mito@tenable.com>
2025-05-21 13:47:33 +0900

    Demo preparation

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -23,7 +23,7 @@ EOF
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/ec2.tf"
     git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "mito@tenable.com"
+    git_last_modified_by = "someone@tenable.com"
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
     git_repo             = "terragoat"
